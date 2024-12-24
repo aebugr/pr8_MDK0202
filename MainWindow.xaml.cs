c774a3f1acb0e8eca0513c0b6733e6637500a5c3
aebugr <aebugr@gmail.com>
2024-12-25 00:27:11 +0500

    Разработан асинхронный метод, который выполняет GET-запрос и обрабатывает ответ. Бугрина

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +22,9 @@ namespace Weather_Bugrina
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly HttpClient client = new HttpClient();
+        private const string ApiKey = "eb4eef9a51c416fe612aea9109273d9c";
+        private const string ApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q={0}&appid={1}&units=metric&lang=ru";
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +38,40 @@ namespace Weather_Bugrina
         private void UpdateKey(object sender, KeyEventArgs e)
         {
 
+        }
+        private async Task<List<WeatherData>> FetchWeatherData(string city)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = string.Format(ApiUrl, city, ApiKey);
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Ошибка {response.StatusCode}: {response.ReasonPhrase}");
+                }
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                var weatherList = new List<WeatherData>();
+
+                foreach (var item in json.list)
+                {
+                    weatherList.Add(new WeatherData
+                    {
+                        DateTime = Convert.ToDateTime(item.dt_txt).ToString("dd.MM.yyyy HH:mm"),
+                        Temperature = $"{item.main.temp} °C",
+                        Pressure = $"{item.main.pressure} мм рт. ст.",
+                        Humidity = $"{item.main.humidity}%",
+                        WindSpeed = $"{item.wind.speed} м/с",
+                        FeelsLike = $"{item.main.feels_like} °C",
+                        WeatherDescription = item.weather[0].description.ToString(),
+                    });
+                }
+
+                return weatherList;
+            }
         }
     }
 }
