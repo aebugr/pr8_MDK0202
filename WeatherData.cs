a4d8b9d812c64b542db5e7abb0d4fc7095346ba0
aebugr <aebugr@gmail.com>
2024-12-24 23:45:31 +0500

    Созданный класс WeatherData для работы с погодой. Бугрина

---
+++ WeatherData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Bugrina
+{
+    public class WeatherData
+    {
+        public string DateTime { get; set; }
+        public string Temperature { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeed { get; set; }
+        public string FeelsLike { get; set; }
+        public string WeatherDescription { get; set; }
+    }
+}
